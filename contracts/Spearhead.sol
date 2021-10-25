6ab1d45c376ff31a857410b783157c8b3421c582
Andrew Dufresne <adufres@Andrews-MacBook-Pro.local>
2021-10-24 22:46:33 -0600

    added subscription contract. need to figure out how to sort out payments and fees for admins as well as sort out inputting new info upon level ups and also sort level up bug.

--- contracts/Spearhead.sol
+++ contracts/Spearhead.sol
@@ -2,66 +2,495 @@
 pragma solidity ^0.8.0;
 
 
+/**
+ * @dev Wrappers over Solidity's arithmetic operations.
+ *
+ * NOTE: `SafeMath` is generally not needed starting with Solidity 0.8, since the compiler
+ * now has built in overflow checking.
+ */
 library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            uint256 c = a + b;
+            if (c < a) return (false, 0);
+            return (true, c);
+        }
+    }
 
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
+    /**
+     * @dev Returns the substraction of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b > a) return (false, 0);
+            return (true, a - b);
+        }
+    }
 
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a / b;
-    return c;
-  }
+    /**
+     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+            // benefit is lost if 'b' is also tested.
+            // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+            if (a == 0) return (true, 0);
+            uint256 c = a * b;
+            if (c / a != b) return (false, 0);
+            return (true, c);
+        }
+    }
 
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
+    /**
+     * @dev Returns the division of two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a / b);
+        }
+    }
 
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    assert(c >= a);
-    return c;
-  }
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a % b);
+        }
+    }
 
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a * b;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator.
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a % b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {trySub}.
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b <= a, errorMessage);
+            return a - b;
+        }
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a / b;
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting with custom message when dividing by zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryMod}.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a % b;
+        }
+    }
 }
 
 
-contract Ownable {
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
 
-  address public owner;
-  address public manager;
-  address public ownerWallet;
-
-  constructor() {
-    owner = msg.sender;
-    manager = msg.sender;
-    ownerWallet = msg.sender;
-  }
-
-  modifier onlyOwner() {
-    require(msg.sender == owner, "only for owner");
-    _;
-  }
-
-  modifier onlyOwnerOrManager() {
-     require((msg.sender == owner)||(msg.sender == manager), "only for owner or manager");
-      _;
-  }
-
-  function transferOwnership(address newOwner) public onlyOwner {
-    owner = newOwner;
-  }
-
-  function setManager(address _manager) public onlyOwnerOrManager {
-      manager = _manager;
-  }
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
 }
 
 
-contract SpearheadProtocol is Ownable {
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+contract Ownable is Context {
+
+    address public owner;
+    address public manager;
+    address public ownerWallet;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+    * @dev Initializes the contract setting the deployer as the initial owner.
+    */
+    constructor() {
+        owner = msg.sender;
+        manager = msg.sender;
+        ownerWallet = msg.sender;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner, "only for owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    modifier onlyOwnerOrManager() {
+        require((msg.sender == owner)||(msg.sender == manager), "only for owner or manager");
+        _;
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = owner;
+        owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+
+    function setManager(address _manager) public onlyOwnerOrManager {
+        manager = _manager;
+    }
+}
+
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/**
+ * @dev contract to handle automated subscription payments
+ */
+contract SubscriptionService {
+
+    uint public nextPlanId;
+    struct Plan {
+        address merchant;
+        address token;
+        uint amount;
+        uint frequency;
+    }
+    struct Subscription {
+        address subscriber;
+        uint start;
+        uint nextPayment;
+    }
+    mapping(uint => Plan) public plans;
+    mapping(address => mapping(uint => Subscription)) public subscriptions;
+
+    event PlanCreated(address merchant, uint planId, uint date);
+    event SubscriptionCreated(address subscriber, uint planId, uint date);
+    event SubscriptionCancelled(address subscriber, uint planId, uint date);
+    event PaymentSent(address from, address to, uint amount, uint planId, uint date);
+
+    function createPlan(address token, uint amount, uint frequency) external {
+        require(token != address(0), 'Address cannot be null address.');
+        require(amount > 0, 'Amount needs to be > 0');
+        require(frequency > 0, 'Frequency needs to be > 0');
+        plans[nextPlanId] = Plan(
+            msg.sender, 
+            token,
+            amount, 
+            frequency
+        );
+        nextPlanId++;
+    }
+
+    function subscribe(uint planId) external {
+        // IERC20 token = IERC20(plans[planId].token);
+        Plan storage plan = plans[planId];
+        require(plan.merchant != address(0), 'This plan does not exist.');
+
+        // collecting payment within payForLevel and don't need it collected here 
+        // token.transferFrom(msg.sender, plan.merchant, plan.amount);  
+        // emit PaymentSent(
+        //     msg.sender, 
+        //     plan.merchant, 
+        //     plan.amount, 
+        //     planId, 
+        //     block.timestamp
+        // );
+
+        subscriptions[msg.sender][planId] = Subscription(
+            msg.sender, 
+            block.timestamp, 
+            block.timestamp + plan.frequency
+        );
+        emit SubscriptionCreated(msg.sender, planId, block.timestamp);
+    }
+
+    function cancel(uint planId) external {
+        Subscription storage subscription = subscriptions[msg.sender][planId];
+        require(subscription.subscriber != address(0), 'This subscription does not exist.');
+        delete subscriptions[msg.sender][planId]; 
+        emit SubscriptionCancelled(msg.sender, planId, block.timestamp);
+    }
+
+    function pay(address subscriber, uint planId) external {
+        Subscription storage subscription = subscriptions[subscriber][planId];
+        Plan storage plan = plans[planId];
+        IERC20 token = IERC20(plan.token);
+        require(subscription.subscriber != address(0), 'This subscription does not exist.');
+        require(block.timestamp > subscription.nextPayment, 'Payment not due yet.');
+
+        token.transferFrom(subscriber, plan.merchant, plan.amount);  
+        emit PaymentSent(
+            subscriber,
+            plan.merchant, 
+            plan.amount, 
+            planId, 
+            block.timestamp
+        );
+        subscription.nextPayment = subscription.nextPayment + plan.frequency;
+    }
+}
+
+/**
+ * @title Spearhead Protocol is a subscription based crypto payment service that builds 
+ * automated binary affiliate lines generating income based on referrals and automatic deposits.
+ */
+contract SpearheadProtocol is Ownable,SubscriptionService {
     using SafeMath for uint256;
 
     struct UserStruct {
@@ -124,6 +553,8 @@ contract SpearheadProtocol is Ownable {
             users[ownerWallet].currentLevel = i;
             levelExpired[ownerWallet][i] = 55555555555;
         }
+
+        // create subscription plan and subscribe owner to it
     } 
 
     receive () external payable {
@@ -177,6 +608,8 @@ contract SpearheadProtocol is Ownable {
         createdDate[msg.sender] = block.timestamp;
 
         payForLevel(0, 1, msg.sender, ((LEVEL_PRICE[1].mul(adminFee)).div(10**20)));
+
+        // assign user subscription 
 
         emit regLevelEvent(msg.sender, userList[_referrerID], block.timestamp);
     }
